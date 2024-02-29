8ec73ee011b9299f5baf82bd80d674a41e8bf6c7
Parth Patel <parth.patel@nintex.com>
2024-02-29 17:25:24 +1300

    Updated cypress tests

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -6,19 +6,22 @@ describe('To-Do App Tests', () => {
 
   it('Allows me to add tasks', () => {
       const newItem = 'Feed the cat';
-      cy.get('input.new-todo').type(`${newItem}{enter}`);
-      cy.get('ul.todo-list').find('li').should('have.length', 1).and('contain', newItem);
+      cy.get('input[id="todo-input"]').type(`${newItem}{enter}`);
+      cy.get('button[id="add-todo-btn"]').click();
+      cy.get('ul[id="todo-list"]').find('li').should('have.length', 1).and('contain', newItem);
   });
 
   it('Allows me to mark a task as completed', () => {
-      cy.get('input.new-todo').type('Feed the cat{enter}');
-      cy.get('ul.todo-list li').first().find('input.toggle').click();
-      cy.get('ul.todo-list li').first().should('have.class', 'completed');
+      cy.get('input[id="todo-input"]').type('Feed the cat{enter}');
+      cy.get('button[id="add-todo-btn"]').click();
+      cy.get('ul[id="todo-list"] li').first().find('input.toggle').click();
+      cy.get('ul[id="todo-list"] li').first().should('have.class', 'completed');
   });
 
   it('Allows me to delete a task', () => {
-      cy.get('input.new-todo').type('Feed the cat{enter}');
-      cy.get('ul.todo-list li').first().find('button.destroy').invoke('show').click();
-      cy.get('ul.todo-list').should('not.exist');
+      cy.get('input[id="todo-input"]').type('Feed the cat{enter}');
+      cy.get('button[id="add-todo-btn"]').click();
+      cy.get('ul[id="todo-list"] li').first().find('button.destroy').invoke('show').click();
+      cy.get('ul[id="todo-list"]').should('not.exist');
   });
 });
