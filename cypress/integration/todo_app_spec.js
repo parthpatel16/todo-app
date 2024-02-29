a2f22a87cd5e461ff77e5bf5fa5199f6f2b411c7
Parth Patel <parth.patel@nintex.com>
2024-02-29 17:15:32 +1300

    Updated cypress tests

--- cypress/integration/todo_app_spec.js
+++ cypress/integration/todo_app_spec.js
@@ -6,18 +6,18 @@ describe('To-Do App Tests', () => {
 
   it('Allows me to add tasks', () => {
       const newItem = 'Feed the cat';
-      cy.get('input.new-todo').type(`${newItem}{enter}`);
+      cy.get('input.todo-input').type(`${newItem}{enter}`);
       cy.get('ul.todo-list').find('li').should('have.length', 1).and('contain', newItem);
   });
 
   it('Allows me to mark a task as completed', () => {
-      cy.get('input.new-todo').type('Feed the cat{enter}');
+      cy.get('input.todo-input').type('Feed the cat{enter}');
       cy.get('ul.todo-list li').first().find('input.toggle').click();
       cy.get('ul.todo-list li').first().should('have.class', 'completed');
   });
 
   it('Allows me to delete a task', () => {
-      cy.get('input.new-todo').type('Feed the cat{enter}');
+      cy.get('input.todo-input').type('Feed the cat{enter}');
       cy.get('ul.todo-list li').first().find('button.destroy').invoke('show').click();
       cy.get('ul.todo-list').should('not.exist');
   });
